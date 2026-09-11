32a59fe0d98ac84a4a48ee2b07b3e1d3dd4f7bda
Aaryan Vashishta <aaryan.25b01010409@abes.ac.in>
2026-09-11 14:08:52 +0530

    js

--- Lab-3/prg6.js
+++ Lab-3/prg6.js
@@ -1,9 +1,44 @@
+
+// creatre acontact us page fot the product enquiry
+
+import { createReadStream } from "fs";
 import http from "http";
 
 const server = http.createServer((req, res) => {
 
+    res.setHeader("content-type", "text/html");
+    if (req.url === "/") {
 
-})
+        res.write("<h1>Home Page</h1>");
 
 
-server.listen(3000, () => console.log("Server runs in 3000..."));
+        res.end(`
+            <a href="/product">Product</a>
+            <a href="/contact">Contact</a>
+            `);
+    }
+    else if (req.url === "/product") {
+        const stream = createReadStream("product.html", {
+            encoding: "utf-8"
+        });
+
+        stream.pipe(res);
+    }
+
+     else if (req.url === "/contact") {
+        const stream = createReadStream("contact.html", {
+            encoding: "utf-8"
+        });
+
+        stream.pipe(res);
+    }
+    
+    else {
+        res.statusCode = 404;
+        res.end("Not found");
+    }
+});
+
+server.listen(3000, () => {
+    console.log("prg6 is running...");
+});
