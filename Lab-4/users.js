3d0774de684f2eb90547ca231b0c16a33f0205ed
Aaryan Vashishta <aaryan.25b01010409@abes.ac.in>
2026-09-11 15:48:54 +0530

    js

---
+++ Lab-4/users.js
@@ -0,0 +1,9 @@
+// we use In-memory database
+let users = [
+    {id:1,name:'yash gupta',mob:'9387xxxxxx',email:'yash.example@exam.com'},
+    {id:2,name:'Aditya Verma',mob:'9147xxxxxx',email:'verma.example@exam.com'}
+]
+
+let nextId = 3;
+
+export const getUsers = () => users;
